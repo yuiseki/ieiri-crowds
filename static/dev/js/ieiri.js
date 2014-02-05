2f208d8c6b7075c432ed4d0744411dd584a3331f
yuiseki <yuiseki@gmail.com>
2014-02-05 17:28:16 +0900

    iframe 非同期生成

--- static/dev/js/ieiri.js
+++ static/dev/js/ieiri.js
@@ -193,6 +193,27 @@ function loadTweetButtons(done) {
 }
 function loadIframes(done) {
     console.log("loadIframes");
+    move = ["move-right", "move-left"];
+    urlList = [
+        "https://www.nhk.or.jp/jirenma/guest_ieirikazuma.html",
+        "http://www.slideshare.net/sinpost2/20140127-f",
+        "https://www.facebook.com/ieirikazuma",
+        "http://nlab.itmedia.co.jp/nl/articles/1312/19/news127.html",
+        "http://www.kakugo.tv/detail_789.html",
+        "http://alternas.jp/study/it_social/14334",
+        "http://ieiri.net/assets/img/main.jpg",
+        "https://www.facebook.com/groups/339881266150122/"
+    ];
+    $.each(urlList, function(idx, url){
+        iframe = $("<iframe></iframe>");
+        iframe.attr(src, url);
+        iframe.addClass("ieiri-site"+(idx+1));
+        iframe.addClass(move[Math.floor(Math.random() * videoList.length)]);
+        setTimeout(function(){
+            $(".iframe-wrap").append(iframe);
+        }, 1000*idx);
+    })
+     
 }
 
 
